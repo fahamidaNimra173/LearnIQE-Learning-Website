5a9c0325e167444646ac849c4359f1f0949435cc
Fahmida Nimra <fahmidanimra@gmail.com>
2025-07-20 01:10:04 +0600

    last commit of the day, partially implement payment system

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { loadStripe } from '@stripe/stripe-js';
+import PaymentForm from './PaymentForm';
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 const Payment = () => {
     return (
         <div>
-            
+            <Elements stripe={stripePromise}>
+               <PaymentForm></PaymentForm>
+            </Elements>
         </div>
     );
 };
