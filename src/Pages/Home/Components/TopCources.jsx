9965f9f84cfff6494b2a447c7295a95e88c36a09
Fahmida Nimra <fahmidanimra@gmail.com>
2025-09-24 18:26:27 +0600

    updated course card

--- src/Pages/Home/Components/TopCources.jsx
+++ src/Pages/Home/Components/TopCources.jsx
@@ -6,7 +6,8 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import { useNavigate } from 'react-router-dom';
 import useAxiosSecure from '../../../Axios/AxiosSecure';
-
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { FiUsers, FiDollarSign } from 'react-icons/fi';
 const TopCources = () => {
     const axiosSecure = useAxiosSecure();
     const navigate = useNavigate();
@@ -50,31 +51,60 @@ const TopCources = () => {
                             swiper.params.navigation.prevEl = prevRef.current;
                             swiper.params.navigation.nextEl = nextRef.current;
                         }}
-                        className="relative shadow-2xl rounded-2xl"
+                        className="relative "
                     >
                         {courses.map((course) => (
                             <SwiperSlide key={course._id}>
-                                <div className="bg-[#ffffff] rounded-xl overflow-hidden hover:shadow-xl transition duration-300 h-full">
+                                <div className="bg-[#ffffff] rounded-lg overflow-hidden hover:shadow-xl transition duration-300 flex flex-col h-full">
                                     <img
                                         src={course.image}
                                         alt={course.title}
                                         className="w-full h-48 object-cover"
                                     />
-                                    <div className="p-4">
-                                        <h3 className="text-xl font-semibold text-[#070707] mb-2">
-                                            {course.title}
-                                        </h3>
-                                        <p className="text-[#060606] mb-1">
-                                            <strong>Enrollments:</strong> {course.totalEnroll}
-                                        </p>
-                                        <p className="text-[#080808] mb-3">
-                                            <strong>Price:</strong> ৳{course.price}
-                                        </p>
+                                    <div className="p-4 flex-1  flex flex-col items-center justify-center">
+                                        <h3 className="text-xl h-20 my-auto font-extrabold text-[#6c4370] merienda min-h-1/2  text-center mb-3 ">{course.title}</h3>
+
+                                        <div className="flex items-center gap-2 text-gray-700 mb-2">
+                                            <FiUsers className="text-[#6c4370] w-5 h-5" />
+                                            <span className="text-md text-black"><strong className='habibi text-[#6c4370]'>Enrollments:</strong> {course.totalEnroll}</span>
+                                        </div>
+
+                                        <div className="flex items-center gap-2 text-gray-700 mb-3">
+                                            <FiDollarSign className="text-[#6c4370] w-5 h-5" />
+                                            <span className="text-md text-black"><strong className='habibi text-[#6c4370]'>Price:</strong> ৳{course.price}</span>
+                                        </div>
                                         <button
                                             onClick={() => navigate(`/classdetails/${course._id}`)}
-                                            className="mt-2 bg-[#040404] hover:bg-purple-800 text-[#ffffff] font-semibold px-4 py-2 rounded transition"
+                                            className="mt-2  buttonMore"
                                         >
-                                            See More
+                                            <span className="buttonMore__icon-wrapper">
+                                                <svg
+                                                    viewBox="0 0 14 15"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="buttonMore__icon-svg"
+                                                    width="10"
+                                                >
+                                                    <path
+                                                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                                        fill="black"
+                                                    ></path>
+                                                </svg>
+
+                                                <svg
+                                                    viewBox="0 0 14 15"
+                                                    fill="none"
+                                                    width="10"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    class="buttonMore__icon-svg buttonMore__icon-svg--copy"
+                                                >
+                                                    <path
+                                                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                                        fill="currentColor"
+                                                    ></path>
+                                                </svg>
+                                            </span>
+                                            Explore More
                                         </button>
                                     </div>
                                 </div>
@@ -84,18 +114,18 @@ const TopCources = () => {
                 )}
 
                 {/* Custom Arrows at Bottom Center */}
-                <div className="flex justify-center gap-4 mt-6">
+                <div className="flex justify-center gap-8 mt-10">
                     <button
                         ref={prevRef}
-                        className="bg-[#1e8a78] hover:bg-[#14514b] text-white px-4 py-2 rounded-full"
+                        className=" border-4 bg-[#d4f3ed] border-[#937f94] border-dashed hover:bg-[#1e8a78]  p-6  rounded-full"
                     >
-                        &#8592; Prev
+                        <IoIosArrowBack className='w-10 h-10 text-[#937f94]' />
                     </button>
                     <button
                         ref={nextRef}
-                        className="bg-[#1e8a78] hover:bg-[#14514b] text-white px-4 py-2 rounded-full"
+                        className="border-4 bg-[#d4f3ed] border-[#937f94] hover:bg-[#1e8a78]   border-dashed p-6  rounded-full"
                     >
-                    <span></span>
+                        <IoIosArrowForward className='w-10 h-10 text-[#937f94]' />
                     </button>
                 </div>
             </div>
