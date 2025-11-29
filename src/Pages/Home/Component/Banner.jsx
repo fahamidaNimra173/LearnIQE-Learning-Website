f021a142dad7bbf9e5577d3eb899039ebcd84842
Fahmida Nimra <fahmidanimra@gmail.com>
2025-11-29 20:51:44 +0600

    added 2 shapes at left of the banner with a spinning react icon

--- src/Pages/Home/Component/Banner.jsx
+++ src/Pages/Home/Component/Banner.jsx
@@ -1,17 +1,100 @@
 // import { Component } from "@/components/etheral-shadow";
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+import Circle from "./Shared/circles";
+import { Boxes } from "lucide-react";
 
 const Banner = () => {
   return (
     <div className="relative flex min-h-screen w-full flex-col items-start justify-start overflow-hidden">
-      <BackgroundRippleEffect/>
-      <div className="mt-60 w-full">
+      <BackgroundRippleEffect />
+      <Circle></Circle>
+      <Boxes className="text-blue-500 opacity-70    blur-sm bottom-39 left-0  w-[200px] h-[200px] absolute  z-50 "
+       style={{
+        animation:"spin 8s linear infinite",
+         }}
+       ></Boxes>
+      <div className="mt-60 flex flex-col gap-5 w-full">
         <h2 className="relative z-10 mx-auto max-w-4xl text-center text-3xl sm:text-4xl font-bold text-white md:text-6xl lg:text-7xl">
           Empowering <br /> Your Learning Journey
         </h2>
         <p className="relative z-10 mx-auto mt-4 max-w-xl text-center text-white">
           Learn with free courses, smart resources, and the skills you’ve always wanted to master.
         </p>
+        <div className='flex my-5 md:my-10 z-10 items-center justify-center mx-2  gap-1 md:gap-5 lg:gap-10 '>
+          {/* Get Started Button */}
+          <button
+            className="flex z-30 bg-[#d7efff] rounded-[40px] justify-center items-center p-[3px] cursor-pointer border-[3px] border-[#2b88f9] shadow-[inset_0_0_20px_rgba(135,206,250,0.7),0_0_25px_rgba(135,206,250,0.9)] transition-shadow duration-300"
+          // onClick={() => navigate("/your-path")}
+          >
+            <span className="flex-1 flex items-center justify-center text-[#003f8a] text-[15px] sm:text-[20px] font-semibold md:font-bold outfit tracking-wide px-[10px]">
+              Get Started!
+            </span>
+
+            <span className="w-[45px] h-[45px] flex-shrink-0 bg-[#ffe55c] flex items-center justify-center rounded-full border-[3px] border-[#003f8a] group">
+              <svg
+                width="16"
+                height="19"
+                viewBox="0 0 16 19"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="group-hover:animate-[arrow_1s_linear_infinite] transition-all duration-[1.5s]"
+              >
+                <circle cx="1.61321" cy="1.61321" r="1.5" fill="black"></circle>
+                <circle cx="5.73583" cy="1.61321" r="1.5" fill="black"></circle>
+                <circle cx="5.73583" cy="5.5566" r="1.5" fill="black"></circle>
+                <circle cx="9.85851" cy="5.5566" r="1.5" fill="black"></circle>
+                <circle cx="9.85851" cy="9.5" r="1.5" fill="black"></circle>
+                <circle cx="13.9811" cy="9.5" r="1.5" fill="black"></circle>
+                <circle cx="5.73583" cy="13.4434" r="1.5" fill="black"></circle>
+                <circle cx="9.85851" cy="13.4434" r="1.5" fill="black"></circle>
+                <circle cx="1.61321" cy="17.3868" r="1.5" fill="black"></circle>
+                <circle cx="5.73583" cy="17.3868" r="1.5" fill="black"></circle>
+              </svg>
+            </span>
+          </button>
+
+          {/* Learn More Button */}
+          <button className="flex z-30 bg-[#d7efff] rounded-[40px] justify-center items-center p-[3px] cursor-pointer border-[3px] border-[#2b88f9] shadow-[inset_0_0_20px_rgba(135,206,250,0.7),0_0_25px_rgba(135,206,250,0.9)] transition-shadow duration-300"
+          // onClick={() => navigate("/learn-more")}
+          >
+            <span className="flex-1 flex items-center justify-center text-[#003f8a] text-[15px] sm:text-[20px] font-semibold md:font-bold outfit tracking-wide px-[10px]">
+              Learn More
+            </span>
+
+            <span className="w-[45px] h-[45px] flex-shrink-0 bg-[#ffe55c] flex items-center justify-center rounded-full border-[3px] border-[#003f8a] group">
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="group-hover:animate-[arrow_1s_linear_infinite] transition-all duration-[1.5s]"
+              >
+                <path
+                  d="M13 5L20 12L13 19M4 12H20"
+                  stroke="black"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+
+        <style jsx>{`
+                  @keyframes arrow {
+                    0% {
+                      opacity: 0;
+                      margin-left: 0px;
+                    }
+                    100% {
+                      opacity: 1;
+                      margin-left: 10px;
+                    }
+                  }
+                `}</style>
+
       </div>
     </div>
   );
---
+++ src/Pages/Home/Component/Shared/circles.jsx
@@ -0,0 +1,15 @@
+import { Boxes } from "lucide-react";
+
+
+
+
+export default function Circle() {
+    return (
+        <div className="min-w-1/2 absolute top-20 -left-1/3 z-10 min-h-2/3 rotate-45 bg-transparent border-4 border-gray-500 shadow-[0_0_90px_rgba(137,137,250,0.7)] rounded-full ">
+            <div className="min-w-1/3   top-10   -left-1/3 z-15 min-h-screen lg:min-h-80 rotate-90 bg-transparent border-4 border-blue-300 shadow-[inset_0_0_50px_rgba(96,165,250,0.9)] rounded-full ">
+
+            </div>
+           
+        </div>
+    )
+}
