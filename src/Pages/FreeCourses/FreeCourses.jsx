ee85cc61908761d61e78695a00c5c6978a855d65
Fahmida Nimra <fahmidanimra@gmail.com>
2025-12-29 20:02:15 +0600

    Implemented tanstack query to get couses

--- src/Pages/FreeCourses/FreeCourses.jsx
+++ src/Pages/FreeCourses/FreeCourses.jsx
@@ -1,6 +1,22 @@
+import AxiosSecure from '@/Axios/AxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const FreeCourses = () => {
+const axiosSecure=AxiosSecure();
+const navigate=useNavigate();
+const {data:courses=[],isLoading,error}=useQuery({
+    queryKey:["courses"],
+    queryFn: async()=>{
+        const res = await axiosSecure.get("/freeCourseMix");
+        return res.data
+    }
+})
+
+
+
+
     return (
         <div>
             
