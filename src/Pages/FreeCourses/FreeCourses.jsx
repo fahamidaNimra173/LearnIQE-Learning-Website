9d95207f3a1827eb1028d67b05c9d375e5ec6532
Fahmida Nimra <fahmidanimra@gmail.com>
2025-12-30 01:47:10 +0600

    add some design on cards and make them grid and responsive

--- src/Pages/FreeCourses/FreeCourses.jsx
+++ src/Pages/FreeCourses/FreeCourses.jsx
@@ -23,22 +23,30 @@ const FreeCourses = () => {
 
 
     return (
-        <div className='grid-cols-2 grid gap-5 mt-20 px-6'>
+        <div className='grid grid-cols-2 lg:grid-cols-4  gap-5 mt-20 px-6'>
             {courses.map(course => (
 
-                <div className='border-2 p-5'>
-                    <div>
-                        <img src={course?.image} alt="course image" />
-                        <h1 className='text-white'>{course.title}</h1>
-                        <h1 className='text-white'>{course.platform}</h1>
-                        <h1 className='text-white'>{course.language}</h1>
-                        <h1 className='text-white'>{course.Enrollment}</h1>
-                        <h1 className='text-white'>{course.rating}</h1>
-                        <div>
-                            <a href={course.url}></a>
-                            <button className='bg-blue-500 text-white px-5 py-2 w-full font-bold rounded-2xl'>Visit</button>
+                <div className=' '>
+                   
+                        <div className=''>
+                            <img src={course?.image} alt="course image" className='h-full w-full object-cover'  />
                         </div>
-                    </div>
+
+                        <div className='p-1  bg-white text-black rounded-b-2xl'>
+                            <h1 className='text-black'>{course.title}</h1>
+                            <h1 className='text-black'>{course.platform}</h1>
+                            <h1 className='text-black'>{course.language}</h1>
+                            <h1 className='text-black'>{course.Enrollment}</h1>
+                            <h1 className='text-black'>{course.rating}</h1>
+                            <div>
+                                <a href={course.url} target='blank'>
+                                    <button className='bg-blue-500 text-white cursor-pointer px-5 py-2 w-full font-bold rounded-2xl'>Visit</button>
+                                </a>
+
+                            </div>
+                        </div>
+
+                   
                 </div>
 
             ))}
