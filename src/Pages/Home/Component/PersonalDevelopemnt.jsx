43a821fb46a658e774a883e34a71ca9e52a2fe6c
Fahmida Nimra <fahmidanimra@gmail.com>
2026-01-10 23:37:01 +0600

    created platforms component

--- src/Pages/Home/Component/PersonalDevelopemnt.jsx
+++ src/Pages/Home/Component/PersonalDevelopemnt.jsx
@@ -116,13 +116,13 @@ const PersonalDevelopment = () => {
     ];
 
     return (
-        <div className="lg:px-25 px-6 py-16">
+        <div className="lg:px-25 mt-10 px-6 py-16">
             {/* Header Section */}
             <div className="mb-12">
-                <h2 className="text-4xl md:text-5xl font-bold text-white mb-4">
+                <h2 className="text-4xl lg:text-7xl  font-bold text-white mb-4">
                     Personal Development
                 </h2>
-                <p className="text-lg text-gray-400 max-w-3xl">
+                <p className="text-lg text-gray-100 font-mono max-w-3xl">
                     Master the essential skills for career growth. Learn how to build professional resumes, create compelling CVs, and make informed career decisions with expert guidance.
                 </p>
             </div>
---
+++ src/Pages/Home/Component/Platforms.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Platforms = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Platforms;
